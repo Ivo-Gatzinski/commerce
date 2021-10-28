ef479f5720e0d39179b24164fc8ac5fbf0ee8054
Ezhko <ucsd.camp.21@gmail.com>
2021-10-27 21:12:05 -0700

    add through includes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get("/", async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [ { model: Product }, { model: ProductTag }],
+      include: [ { model: Product, through: ProductTag, as: "tag_products" }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }]
+      include: [{ model: Product, through: ProductTag, as: "tag_products" }]
     });
 
     if (!tagData) {
