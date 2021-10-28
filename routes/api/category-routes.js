94d09c285e85a2605f90f21447866c4dd98a9733
Ezhko <ucsd.camp.21@gmail.com>
2021-10-27 21:03:34 -0700

    fix async typo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const categoryData = await Category.update(req.body);
     res.status(200).json(categoryData);
